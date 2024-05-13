af116f044066ed24ecc2474d1a35a394f6bedf01
Claudio Coelho <claudio.coelho@solenix.ch>
2024-05-13 16:32:30 +0300

    void tile pixel offset

--- godot/src/world/void_tile.gd
+++ godot/src/world/void_tile.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
 const SPEED_VARIABILITY := .25
+const PIXEL_OFFSET := 2
 @onready var sprite: AnimatedSprite2D = $Sprite
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var frame_count := sprite.sprite_frames.get_frame_count("default")
-	
+	sprite.offset = Vector2(randi_range(-PIXEL_OFFSET,PIXEL_OFFSET), randi_range(-PIXEL_OFFSET,PIXEL_OFFSET))
 	var speed_delta := randf_range(-SPEED_VARIABILITY, SPEED_VARIABILITY)
 	sprite.speed_scale += speed_delta
 	sprite.frame = randi() % frame_count
