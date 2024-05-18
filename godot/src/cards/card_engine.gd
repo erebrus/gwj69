5c628092c311dff94250086726b46b6b8229024a
Claudio Coelho <claudio.coelho@solenix.ch>
2024-05-18 14:11:49 +0300

    discard from hand if creating card when hand is full

--- godot/src/cards/card_engine.gd
+++ godot/src/cards/card_engine.gd
@@ -18,7 +18,8 @@ func draw(num_cards := 1, manual:=true):
 		super.draw(1, manual)
 		$sfx_draw.play()
 		await get_tree().create_timer(.2).timeout
-		
+
+
 func _on_discard_requested(card_ui:CardUI):
 	set_card_pile(card_ui, CardPileUI.Piles.discard_pile)
 	
@@ -44,7 +45,13 @@ func set_state(state):
 			remove_card_from_game(card)
 		for card in state[pile]:
 			create_card_in_pile(card, pile)
-	
+			
+func create_card_in_pile(nice_name : String, pile_to_add_to : Piles):
+	if pile_to_add_to==Piles.hand_pile:
+		if _hand_pile.size() == max_hand_size:
+			_on_discard_requested(_hand_pile[0])
+	super.create_card_in_pile(nice_name, pile_to_add_to)
+	$sfx_draw.play()
 
 func _on_card_destroy_requested(card:CardUI):
 	_maybe_remove_card_from_any_piles(card)
