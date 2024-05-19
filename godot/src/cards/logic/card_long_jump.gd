c9926f5fb27875486332b0b6741de9e778197a8d
Ángela <angela.lbsr@gmail.com>
2024-05-19 18:21:00 +0200

    dont play player cards when player dead

--- godot/src/cards/logic/card_long_jump.gd
+++ godot/src/cards/logic/card_long_jump.gd
@@ -1,5 +1,10 @@
 extends CustomCardUIData
 #TODO implement or remove
+
+func can_play():
+	return Globals.player_alive
+	
+
 func _do_play():
 	pass
 	#Events.long_jump_requested.emit()
--- godot/src/cards/logic/card_turn_around.gd
+++ godot/src/cards/logic/card_turn_around.gd
@@ -1,4 +1,8 @@
 extends CustomCardUIData
 
+func can_play():
+	return Globals.player_alive
+	
+
 func _do_play():
 	Events.turn_around_requested.emit()
