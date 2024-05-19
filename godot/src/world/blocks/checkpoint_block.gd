e613820fc9d561be86fd737a41495be1fd96e7db
forager-pixel <forager.pixel@gmail.com>
2024-05-18 23:31:26 -0400

    Checkpoint fade-in + dirt particles

--- godot/src/world/blocks/checkpoint_block.gd
+++ godot/src/world/blocks/checkpoint_block.gd
@@ -4,9 +4,12 @@ class_name CheckPoint extends BaseBlock
 var card_engine_state: Dictionary
 var world_state: Dictionary
 
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+
 func _ready() -> void:
 	# checkpoint block should never be timed
 	timed = false
+	anim_player.play("Place")
 	
 func get_state() -> Dictionary:
 	return {}
